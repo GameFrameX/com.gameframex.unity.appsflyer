4c62917ff52cfb5639172108b2cf8d7ed05fe42e
Blank <wangfj11@foxmail.com>
2025-10-23 16:31:27 +0800

    feat(AppsFlyer): 扩展初始化方法以支持调试模式和配置参数
    添加调试模式开关、应用ID、开发者密钥和MonoBehaviour参数到初始化方法
    实现AppsFlyer SDK的完整初始化流程，包括调试设置、用户同意数据处理和设备ID设置

--- Runtime/AppsFlyerManager.cs
+++ Runtime/AppsFlyerManager.cs
@@ -4,7 +4,11 @@
 // 
 // 不得利用本项目从事危害国家安全、扰乱社会秩序、侵犯他人合法权益等法律法规禁止的活动！任何基于本项目二次开发而产生的一切法律纠纷和责任，我们不承担任何责任！
 
+#if ENABLE_GAME_FRAME_X_APPSFLYER
+using AppsFlyerSDK;
+#endif
 using GameFrameX.Runtime;
+using UnityEngine;
 
 namespace GameFrameX.AppsFlyer.Runtime
 {
@@ -16,9 +20,31 @@ namespace GameFrameX.AppsFlyer.Runtime
         {
         }
 
+        /// <summary>
+        /// 初始化 AppsFlyer SDK    
+        /// </summary>
+        /// <param name="debug">是否启用调试模式，启用后会输出详细的日志信息</param>
+        /// <param name="appId">应用程序ID，iOS平台为App Store ID，Android平台为包名</param>
+        /// <param name="appDevKey">AppsFlyer开发者密钥，从AppsFlyer控制台获取</param>
+        /// <param name="monoBehaviour">MonoBehaviour实例，用于处理回调事件</param>
         [UnityEngine.Scripting.Preserve]
-        public void Init()
+        public void Init(bool debug, string appId, string appDevKey, MonoBehaviour monoBehaviour)
         {
+#if ENABLE_GAME_FRAME_X_APPSFLYER
+            if (debug)
+            {
+                AppsFlyerSDK.AppsFlyer.setIsDebug(true);
+            }
+
+            AppsFlyerSDK.AppsFlyer.initSDK(appDevKey, appId, monoBehaviour);
+
+            AppsFlyerConsent consent = new AppsFlyerConsent(true, true);
+
+            AppsFlyerSDK.AppsFlyer.setConsentData(consent);
+            AppsFlyerSDK.AppsFlyer.handlePushNotifications();
+            AppsFlyerSDK.AppsFlyer.setCustomerUserId(SystemInfo.deviceUniqueIdentifier);
+            AppsFlyerSDK.AppsFlyer.startSDK();
+#endif
         }
 
         protected override void Update(float elapseSeconds, float realElapseSeconds)
--- Runtime/IAppsFlyerManager.cs
+++ Runtime/IAppsFlyerManager.cs
@@ -4,6 +4,8 @@
 // 
 // 不得利用本项目从事危害国家安全、扰乱社会秩序、侵犯他人合法权益等法律法规禁止的活动！任何基于本项目二次开发而产生的一切法律纠纷和责任，我们不承担任何责任！
 
+using UnityEngine;
+
 namespace GameFrameX.AppsFlyer.Runtime
 {
     /// <summary>
@@ -13,9 +15,13 @@ namespace GameFrameX.AppsFlyer.Runtime
     public interface IAppsFlyerManager
     {
         /// <summary>
-        /// 初始化
+        /// 初始化 AppsFlyer SDK    
         /// </summary>
+        /// <param name="debug">是否启用调试模式，启用后会输出详细的日志信息</param>
+        /// <param name="appId">应用程序ID，iOS平台为App Store ID，Android平台为包名</param>
+        /// <param name="appDevKey">AppsFlyer开发者密钥，从AppsFlyer控制台获取</param>
+        /// <param name="monoBehaviour">MonoBehaviour实例，用于处理回调事件</param>
         [UnityEngine.Scripting.Preserve]
-        void Init();
+        void Init(bool debug, string appId, string appDevKey, MonoBehaviour monoBehaviour);
     }
 }
